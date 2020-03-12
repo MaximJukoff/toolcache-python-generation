852706df71a468d215c99ddbba606387a5ba7da8
Vladimir Safonkin <vsafonkin@gmail.com>
2020-03-12 14:14:15 +0300

    fix

--- tests/sources/Test.Helpers.psm1
+++
@@ -1,9 +0,0 @@
-function Get-CommandExitCode {
-    Param (
-      [String] [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()]
-      $Command
-    )
-  
-    $null = Invoke-Expression -Command $Command
-    return $LASTEXITCODE
-}
