6b8a1a07fc1c65c407dc8844d09558cfb7f27b3e
Vladimir Safonkin <vsafonkin@gmail.com>
2020-03-12 11:37:15 +0300

    Refactoring

--- tests/sources/Python.Tests.ps1
+++ tests/sources/Python.Tests.ps1
@@ -7,7 +7,7 @@ param (
     $ToolsDirectory
 )
 
-function InvokePythonCode {
+function Get-CommandExitCode {
     Param (
       [String] [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()]
       $Command
@@ -20,46 +20,44 @@ function InvokePythonCode {
 Describe "Python toolcache tests" {
 
     It "Python version" {
-        & python --version
-        & which python
-        $whichPython = & which python
+        Get-CommandExitCode "python --version" | Should -Be 0
+        $pythonLocation = (Get-Command "python").Path
+        $pythonLocation | Should -Not -BeNullOrEmpty
         $expectedPath = "$ToolsDirectory/Python"
         if ($Platform -eq 'windows') {
             $expectedPath = $expectedPath.Replace("C:/", "/c/")
         }
-        $whichPython.startsWith($expectedPath) | Should -BeTrue
+        $pythonLocation.startsWith($expectedPath) | Should -BeTrue
     }
 
     It "Run sample code" {
-        InvokePythonCode -Command "python ./main.py" | Should -Be 0
+        Get-CommandExitCode -Command "python ./main.py" | Should -Be 0
     }
 
     It "Validate modules"  {
         if ($Platform -notmatch "windows") {
-            InvokePythonCode -Command "python ./python_modules.py" | Should -Be 0
+            Get-CommandExitCode -Command "python ./python_modules.py" | Should -Be 0
         }
     }
 
     It "Check Tkinter" {
         if ($Platform -notmatch "windows") {
-            InvokePythonCode -Command "python ./check_tkinter.py" | Should -Be 0
+            Get-CommandExitCode -Command "python ./check_tkinter.py" | Should -Be 0
         }
     }
 
     It "Validate Pyinstaller" {
         # Pyinstaller 3.5 does not support Python 3.8.0. Check issue https://github.com/pyinstaller/pyinstaller/issues/4311
         if ($Version -lt "3.8.0") {
-            Invoke-Expression "pip install pyinstaller"
-            InvokePythonCode -Command "pyinstaller --onefile ./main.py" | Should -Be 0
-            Invoke-Expression "./dist/main"
-            $LASTEXITCODE | Should -Be 0
+            Get-CommandExitCode "pip install pyinstaller" | Should -Be 0
+            Get-CommandExitCode -Command "pyinstaller --onefile ./main.py" | Should -Be 0
+            Get-CommandExitCode "./dist/main" | Should -Be 0
         }
     }
 
     It "Pip psutil installation" {
         if ($Platform -notmatch "windows") {
-            Invoke-Expression "bash ./psutil_install_test.sh" 
-            $LASTEXITCODE | Should -Be 0
+            Get-CommandExitCode "bash ./psutil_install_test.sh" | Should -Be 0
         }
     }
 }
