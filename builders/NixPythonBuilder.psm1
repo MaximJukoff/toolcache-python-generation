7edea04ace6db94b3db182c41729ede571fd7e36
Maxim Lobanov <v-malob@microsoft.com>
2020-03-24 11:35:24 +0300

    Update NixPythonBuilder.psm1

--- builders/NixPythonBuilder.psm1
+++ builders/NixPythonBuilder.psm1
@@ -41,7 +41,8 @@ class NixPythonBuilder : PythonBuilder {
 
         Write-Host "Sources URI: $sourceUri"
         Download-Source -Uri $sourceUri -OutFile $pythonSourceLocation
-        Unpack-TarArchive -OutFile $pythonSourceLocation -ExpandArchivePath $this.TempFolderLocation
+        tar -C "$($this.TempFolderLocation)" -xvzf "$pythonSourceLocation"
+        #Unpack-TarArchive -OutFile $pythonSourceLocation -ExpandArchivePath $this.TempFolderLocation
         $expandedSourceLocation = Join-Path -Path $this.TempFolderLocation -ChildPath "Python-$($this.Version)"
         Write-Debug "Done; Sources location: $expandedSourceLocation"
 
