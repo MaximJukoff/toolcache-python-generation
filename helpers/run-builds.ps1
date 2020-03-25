96afeeeecd61986e76aa8ea74e337130b7b5a27e
Maksim Zhukov <v-mazhuk@microsoft.com>
2020-03-25 16:20:20 +0300

    Configure CI for PRs

---
+++ helpers/run-builds.ps1
@@ -0,0 +1,43 @@
+param (
+    [Parameter(Mandatory)] [string] $AzDoOrganizationName,
+    [Parameter(Mandatory)] [string] $AzDoProjectName,
+    [Parameter(Mandatory)] [string] $AzDoAccessToken,
+    [Parameter(Mandatory)] [string] $SourceBranch,
+    [Parameter(Mandatory)] [string] $PythonVersions,
+    [Parameter(Mandatory)] [UInt32] $BuildId
+)
+
+function Get-RequestParams {
+    param (
+        [Parameter(Mandatory)] [string] $PythonVersion
+    )
+
+    $encodedToken = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("'':${AzDoAccessToken}"))
+
+    $body = @{
+        definition = @{
+            id = $BuildId
+        }
+        sourceBranch = $SourceBranch
+        # The content of parameters field should be a json string
+        parameters = @{ VERSION = $PythonVersion } | ConvertTo-Json -Depth 2
+    } | ConvertTo-Json -Depth 3
+
+    return @{
+        Method = "POST"
+        ContentType = "application/json"
+        Uri = "https://dev.azure.com/${AzDoOrganizationName}/${AzDoProjectName}/_apis/build/builds?api-version=5.1"
+        Headers = @{
+            Authorization = "Basic $encodedToken"
+        }
+        Body = $body
+    }
+}
+
+$PythonVersions.Split(',') | ForEach-Object { 
+    $version = $_.Trim()
+    $requestParams = Get-RequestParams -PythonVersion $version
+    Write-Host "Queue build for Python $version..."
+    $response = Invoke-RestMethod @requestParams
+    Write-Host "Queued build: $($response._links.web.href)"
+}
